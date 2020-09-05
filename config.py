3feae52cdbb1f2f1ca264082f4f04d8e2b28adbb
William Ardianto <william.ardianto@gmail.com>
2020-09-05 23:47:00 +0800

    add config

---
+++ config.py
@@ -0,0 +1,11 @@
+
+
+class Config():
+    def __init__(self):
+        self.lr =0.01
+        self.class_num = 10575
+        self.batch_size = 256
+        self.device = 'cpu'
+        self.print_every = 10
+
+        
